29a56f28e48153f48b5de7948b216c1deab5be28
Miroslaw Stypula <mirostype@gmail.com>
2018-06-15 18:23:40 +0200

    Initial Commit

---
+++ GitRepotest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitRepotest
+//
+//  Created by Mac on 15/06/2018.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
